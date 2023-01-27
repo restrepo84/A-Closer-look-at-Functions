6b07ab8530bd5e1230d7ddc860233a1412eb89ee
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-01-27 10:52:03 -0700

    The bind Method

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ const eurowings = {
 const book = lufthansa.book;
 
 // Does NOT work
-//book(23, "Jessica William");
+// book(23, "Jessica William");
 
 // Call method
 book.call(eurowings, 23, "Jessica Williams");
@@ -163,3 +163,50 @@ console.log(swiss);
 
 // this is a better way to call it then apply
 book.call(swiss, ...flightData);
+
+//* The Bind Method
+
+// book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, "Steven Restrepo");
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Jessica William");
+bookEW23("Martha Cooper");
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
